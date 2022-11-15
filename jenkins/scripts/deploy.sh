80aa6c31b2685305198eaee19ff1f9cee0cc409c
Jun Kiat Ng <ngjunkiat968@gmail.com>
2022-11-15 20:56:53 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 set -x
-docker run --rm -p 8000:80 --name my-apache-php-app -v //c/Users/ngjun/Downloads/blueoceanLab/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+docker run --rm -p 80:80 --name my-apache-php-app -v //c/Users/ngjun/Downloads/blueoceanLab/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost to see your PHP application in action.'
+echo 'Visit http://xxxxxxxxx to see your PHP application in action.'
 
