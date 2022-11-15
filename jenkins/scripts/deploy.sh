645ff41f0147b258dfdaf630aa849bd738b054e9
Jun Kiat Ng <ngjunkiat968@gmail.com>
2022-11-15 18:17:42 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run --rm -p 8000:80 --name my-apache-php-app -v c:/Users/ngjun/Downloads/blueoceanLab/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+docker run --rm -p 8000:80 --name my-apache-php-app -v //c/Users/ngjun/Downloads/blueoceanLab/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
